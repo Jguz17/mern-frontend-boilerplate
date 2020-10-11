a4ff8cc5aa3e12a55a0707a5f03c0f35ee49389d
jguz17 <johnathanguzman17@gmail.com>
2020-10-11 02:06:03 -0500

    initial context structure

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 import Navbar from './components/layout/Navbar'
 import Home from './components/pages/Home'
 import Login from './components/pages/Login'
 import Register from './components/pages/Register'
 import About from './components/pages/About'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import AuthState from './context/auth/AuthState'
+
 import './App.css';
 
 const App = () => {
   return (
-    <Router>
-      <Fragment>
-        <Navbar/>
-        <div className='container'>
-          <Switch>
-            <Route exact path='/' component={Home}/>
-            <Route exact path='/signin' component={Login}/>
-            <Route exact path='/signup' component={Register}/>
-            <Route exact path='/about' component={About}/>
-          </Switch>
-        </div>
-      </Fragment>
-    </Router>
+    <AuthState>
+      <Router>
+        <Fragment>
+          <Navbar/>
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={Home}/>
+              <Route exact path='/signin' component={Login}/>
+              <Route exact path='/signup' component={Register}/>
+              <Route exact path='/about' component={About}/>
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+    </AuthState>
+    
   );
 }
 
